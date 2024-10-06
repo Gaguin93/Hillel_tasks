641878c1ab7f7b25f97df20996e46d9f795f6a98
Gaguin93 <83220721+Gaguin93@users.noreply.github.com>
2024-10-06 07:59:43 +0300

    Delete Task5(2).js

--- Task5(2).js
+++
@@ -1,7 +0,0 @@
-    // курс долара до гривні
-const exchangeRate = 26;
-
-for (let dollars = 10; dollars <=100; dollars +=10) {
-    const result = dollars * exchangeRate;
-    console.log(`${dollars} доларів = ${result} гривень`);
-}
